65f08b5fd7ded74ef9b428c04d9164f5fd51741a
sachin <sachin5565y@gmail.com>
2023-11-04 00:22:01 +0530

    database

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')  // Mongoose return promise 
-const mongoToUri = "mongodb://0.0.0.0:27017/iNotebook";
+const mongoToUri = "mongodb+srv://sachin5565y:8Gz2uZAJp1pjVqKx@cluster0.kxasuho.mongodb.net/?retryWrites=true&w=majority";
 const connectToMongo = () => {
     mongoose.connect(mongoToUri).then(() => {
         console.log("connected to mongo successfuly");       // We can use async/await, if dont want use callback function    
